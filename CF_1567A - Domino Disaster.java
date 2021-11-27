e012eb6480f1bae00e0db9d6259414b8f144c706
Se-young Hwang <nahwasa@gmail.com>
2021-11-27 17:04:48 +0900

    CR #742 - A. Domino Disaster
    해설은 nahwasa.com 블로그에!

---
+++ CF_1567A - Domino Disaster.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        initFI();
+        int t = nextInt();
+        StringBuilder sb = new StringBuilder();
+        while (t-->0) {
+            int n = nextInt();
+            String s = nextLine();
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                if (c == 'D') sb.append('U');
+                else if (c == 'U') sb.append('D');
+                else sb.append(c);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+ 
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+ 
+    private static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+ 
+    private static String nextLine() throws IOException {
+        byte[] buf = new byte[64]; // line length
+        int cnt = 0, c;
+        while ((c = read()) != -1) {
+            if (c == '\n') {
+                if (cnt != 0) break;
+                continue;
+            }
+            buf[cnt++] = (byte)c;
+        }
+        return new String(buf, 0, cnt);
+    }
+ 
+    private static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+ 
+    private static long nextLong() throws IOException {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+ 
+    private static double nextDouble() throws IOException {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (c == '.') while ((c = read()) >= '0' && c <= '9') ret += (c - '0') / (div *= 10);
+        if (neg) return -ret;
+        return ret;
+    }
+ 
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
